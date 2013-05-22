185369372a69b46a26de2623ef813339515c2b0e
Kam Woods <kamwoods@Kams-MacBook-Air.local>
2013-05-22 18:07:22 -0400

    Merged optimizations, GUI alpha

--- python/bc_gen_feature_rep_xls.py
+++ python/bc_gen_feature_rep_xls.py
@@ -15,10 +15,11 @@ from openpyxl.cell import get_column_letter
 
 def bc_generate_feature_xlsx(PdfReport, data, feature_file):
 
-    wb = Workbook()
+    wb = Workbook(optimized_write = True)
     dest_filename = PdfReport.featuredir +'/'+ (filename_from_path(feature_file))[10:-3] + "xlsx"
     row_idx = [2]
-    ws = wb.worksheets[0]
+    ##ws = wb.worksheets[0]
+    ws = wb.create_sheet()
     ws.title = "File Feature Information"
 
     ws.cell('%s%s'%('A', '1')).value = '%s' % "Filename"
