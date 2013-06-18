4970394c6f665e340c0b98b5f145314ab14a1d1c
BitCurator <bcadmin@ubuntu.(none)>
2013-06-17 22:47:44 -0400

    Modified report generator to handle line changes in annotated files.

--- python/generate_report.py
+++ python/generate_report.py
@@ -835,11 +835,11 @@ class PdfReport:
             for line in lines:
                 linenumber+=1
 
-                ## print("D: Line: ", line[2:])
-                bc_utils.match_and_write(of, line[2:], "Total features input", 1)
-                bc_utils.match_and_write(of, line[2:], "Total features located to files", 1)
-                bc_utils.match_and_write(of, line[2:], "Total features in unallocated space" ,1)
-                bc_utils.match_and_write(of, line[2:], "Total features in compressed regions", 0)
+                ##print("D: Line: ", line[2:])
+                bc_utils.match_and_write(of, line[:], "Total features input", 1)
+                bc_utils.match_and_write(of, line[:], "Total features located to files", 1)
+                bc_utils.match_and_write(of, line[:], "Total features in unallocated space" ,1)
+                bc_utils.match_and_write(of, line[:], "Total features in compressed regions", 0)
 
                 if ((fnmatch.fnmatch(line, 'Total*') or
                     (fnmatch.fnmatch(line, 'Unicode*')))):
