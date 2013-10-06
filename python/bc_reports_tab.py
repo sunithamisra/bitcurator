e7e35d86472bd89eea3f38b7df6ca09a0ebcdf1f
sunitha <sunithanc@yahoo.com>
2013-10-06 19:44:22 -0400

    Removed the hardcoded home directory used for test purpose.

--- python/bc_reports_tab.py
+++ python/bc_reports_tab.py
@@ -1389,7 +1389,7 @@ class Ui_MainWindow(object):
         self.allrepBcpyDir = "/home/bcadmin/Tools/bulk_extractor/python"
         # FIXME: For testing, I have set it to my path. Replace this
         # line with the line above before committing.
-        self.allrepBcpyDir = "/home/sunitha/Research/Tools/bulk_extractor/python"
+        # self.allrepBcpyDir = "/home/sunitha/Research/Tools/bulk_extractor/python"
         return (0)
 
 
